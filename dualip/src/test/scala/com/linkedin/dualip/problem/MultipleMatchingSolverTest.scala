a5643fda40495f4bc12301a74afe0245a89be7ef
Ayan Acharya <ayachary@ayachary-mn3.linkedin.biz>
2023-05-16 22:46:54 -0700

    adding multiple matching solver

--- dualip/src/test/scala/com/linkedin/dualip/problem/MultipleMatchingSolverTest.scala
+++ dualip/src/test/scala/com/linkedin/dualip/problem/MultipleMatchingSolverTest.scala
@@ -1,6 +1,7 @@
 package com.linkedin.dualip.problem
 
 import breeze.linalg.{SparseVector => BSV}
+import com.linkedin.dualip.data.MultipleMatchingData
 import com.linkedin.dualip.maximizer.solver.firstorder.gradientbased.AcceleratedGradientDescent
 import com.linkedin.dualip.projection.{BoxCutProjection, GreedyProjection, SimplexProjection}
 import com.linkedin.dualip.slate.MultipleMatchingSlateComposer
